c1685f5627700abdabc092202185e39f97e0d12a
Phillip Lord <phillip.lord@russet.org.uk>
2019-05-29 22:17:33 +0200

    Add some command line tools

---
+++ src/bin/raptor-dump.rs
@@ -0,0 +1,49 @@
+extern crate clap;
+#[macro_use]
+extern crate failure;
+extern crate raptor_rs;
+
+use clap::App;
+use clap::Arg;
+
+use failure::Error;
+
+use std::fs;
+
+use raptor_rs::*;
+
+#[derive(Debug, Fail)]
+pub enum CommandError {
+    #[fail(display = "An argument that was expected is missing")]
+    MissingArgument,
+}
+
+fn main() -> Result<(), Error> {
+    //Hello
+    let matches = App::new("raptor-parse")
+        .version("0.1")
+        .about("Parse an RDF file")
+        .author("Phillip Lord")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input = matches
+        .value_of("INPUT")
+        .ok_or(CommandError::MissingArgument)?;
+
+    let m = MemoryParserHandler::new();
+    let mut p = Parser::new("rdfxml", "http://www.example.com", &m);
+
+    let st = fs::read_to_string(input)?;
+
+    p.parse_chunk(&st);
+    p.parse_complete();
+
+    dbg!(m.0);
+    Ok(())
+}
